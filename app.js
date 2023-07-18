7705efc938867bcc691e2d74ce6c81b6234e88a8
Pranjal <bahuguna.pranjal@gmail.com>
2023-07-18 10:21:04 -0500

    removed bloated code

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ async function monitorContract() {
         toBlock:receipt.blockNumber,
         excludeZeroValue: true,
         category: ["internal"],
+        fromAddress:"0xb2ecfe4e4d61f8790bbb9de2d1259b9e2410cea5"
       })
 
       if (assetTransfers == null) {
@@ -176,18 +177,8 @@ async function monitorContract() {
               }
             }
         }
-        if(recipient.toLowerCase() != '0xb2ecfe4e4d61f8790bbb9de2d1259b9e2410cea5'){
-          for(let log of assetTransfers.transfers){
-            if((log.from == '0x39da41747a83aee658334415666f3ef92dd0d541' && log.to == '0xb2ecfe4e4d61f8790bbb9de2d1259b9e2410cea5')){
-              totalPrice = totalPrice + log.value
-            }
-          }
-        }else if(recipient.toLowerCase() == '0xb2ecfe4e4d61f8790bbb9de2d1259b9e2410cea5'){
-          for(let log of assetTransfers.transfers){
-            if((log.from == '0xb2ecfe4e4d61f8790bbb9de2d1259b9e2410cea5')){
-              totalPrice = totalPrice + log.value
-            }
-          }
+        for(let log of assetTransfers.transfers){
+          totalPrice = totalPrice + log.value
         }
 
         tokens = _.uniq(tokens);
